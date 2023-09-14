da4afd51b88cceb9847817471521750ce041f361
Everett <brothers.everett@yahoo.com>
2023-09-14 08:12:44 -0600

    Finished calculator lab

--- Calculator - Lab/Calculator - Lab/ViewController.swift
+++ Calculator - Lab/Calculator - Lab/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
     @IBOutlet var allButtons: [UIButton]!
-    var equation: Equation!
+    var equation = Equation(first: 0.0, second: 0.0, type: "")
+    var check = true
     
     var numArray: [String: Double] = [:]
     override func viewDidLoad() {
@@ -21,65 +22,79 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         var intLabel: Double = Double(label.text!) ?? 0
-        var intString = label.text
+        var str = label.text
         switch sender.titleLabel?.text {
         case "Clear":
-            intString = "0"
+            str = "0"
+            check = false
         case "+/-":
-            if intLabel / -1 == -intLabel {
-                label.text = "-\(label.text ?? "0")"
-            } else {
-                label.text = "+\(label.text ?? "0")"
-            }
+            intLabel = -1 * intLabel
+            str = String(intLabel)
         case "%":
             intLabel *= 0.01
-            label.text = String(intLabel)
+            str = String(intLabel)
         case "÷":
             equation.first = intLabel
             equation.type = "divide"
-            label.text = "0"
+            str = "0"
         case "X":
             equation.first = intLabel
             equation.type = "multiply"
-            label.text = "0"
+            str = "0"
         case "-":
             equation.first = intLabel
             equation.type = "subtract"
-            label.text = "0"
+            str = "0"
         case "+":
             equation.first = intLabel
             equation.type = "add"
-            label.text = "0"
+            str = "0"
         case "1":
-            intString?.append("1")
+            str?.append("1")
+            check = true
         case "2":
-            label.text = "\(label.text ?? "0")2"
+            str?.append("2")
+            check = true
         case "3":
-            label.text = "\(label.text ?? "0")3"
+            str?.append("3")
+            check = true
         case "4":
-            label.text = "\(label.text ?? "0")4"
+            str?.append("4")
+            check = true
         case "5":
-            label.text = "\(label.text ?? "0")5"
+            str?.append("5")
+            check = true
         case "6":
-            label.text = "\(label.text ?? "0")6"
+            str?.append("6")
+            check = true
         case "7":
-            label.text = "\(label.text ?? "0")7"
+            str?.append("7")
+            check = true
         case "8":
-            label.text = "\(label.text ?? "0")8"
+            str?.append("8")
+            check = true
         case "9":
-            label.text = "\(label.text ?? "0")9"
+            str?.append("9")
+            check = true
         case "0":
-            label.text = "\(label.text ?? "0")0"
+            str?.append("0")
+            check = true
         case ".":
-            label.text = "\(label.text ?? "0")."
+            str?.append(".")
+            check = false
         case "=":
             equation.second = intLabel
             equation.solve()
-            label.text = String(equation.total)
+            str = String(equation.total)
         default:
             print("Error with button")
         }
-        label.text = String(Int(intString!)!)
+        if label.text == "0.0" && check {
+            str?.removeFirst(3)
+        } else if label.text == "0" && check {
+            str?.removeFirst()
+        }
+        label.text = str
     }
     
 }
